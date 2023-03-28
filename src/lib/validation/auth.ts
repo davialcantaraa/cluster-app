ee24d02a83e7cd0781d41b01a78f6393515086dc
davialcantaraa <dxvialcantara@gmail.com>
2023-03-28 16:21:41 -0300

    feat: finished sign in and sign up page

--- src/lib/validation/auth.ts
+++ src/lib/validation/auth.ts
@@ -5,4 +5,15 @@ export const signUpSchema = z.object({
   password: z.string().min(4).max(12),
 });
 
+export const signInFirstStepSchema = z.object({
+  email: z.string().email(),
+});
+
+export const signInSecondStepSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4).max(12),
+});
+
 export type ISignUp = z.infer<typeof signUpSchema>;
+export type ISignInFirstStepSchema = z.infer<typeof signInFirstStepSchema>;
+export type ISignInSecondStepSchema = z.infer<typeof signInSecondStepSchema>;
