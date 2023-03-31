206c888100f067add63958c2cbf63e38f7b42ef3
davialcantaraa <dxvialcantara@gmail.com>
2023-03-30 22:22:15 -0300

    feat: add user-menu component

---
+++ src/components/ui/user-menu.tsx
@@ -0,0 +1,84 @@
+import { ChevronUp } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { userMenuItems } from "~/config/user-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+
+export const UserMenu = () => {
+  return (
+    <div className="w-full border-t px-4 py-6">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="p-2">
+          <button className="flex w-full items-center justify-between rounded-md transition-colors hover:bg-gray-100 focus:outline-none">
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                  className="rounded-full"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <p>Davi Alcântara</p>
+            </div>
+            <ChevronUp size={16} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {userMenuItems.map((group, index) => (
+            <>
+              <DropdownMenuGroup key={group.id}>
+                {group.items.map((item) =>
+                  item.subItems ? (
+                    <DropdownMenuSub key={item.value}>
+                      <DropdownMenuSubTrigger>
+                        {item.icon}
+                        <span>{item.label}</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          {item.subItems.map((subItem) => (
+                            <DropdownMenuItem key={subItem.value}>
+                              {subItem.icon}
+                              <span>{subItem.label}</span>
+                            </DropdownMenuItem>
+                          ))}
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                  ) : (
+                    <DropdownMenuItem key={item.value}>
+                      {item.icon}
+                      <span>{item.label}</span>
+                      {item.shortcut && (
+                        <DropdownMenuShortcut>
+                          {item.shortcut}
+                        </DropdownMenuShortcut>
+                      )}
+                    </DropdownMenuItem>
+                  )
+                )}
+              </DropdownMenuGroup>
+              {index !== userMenuItems.length && <DropdownMenuSeparator />}
+            </>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
