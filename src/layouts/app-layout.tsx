82ab549b882a9fb9ac50e813aecab92099555c42
davialcantaraa <dxvialcantara@gmail.com>
2023-04-01 00:08:03 -0300

    feat: add hotkeys package

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,8 +1,16 @@
+import { useHotkeys } from "@mantine/hooks";
 import { PropsWithChildren } from "react";
 import { Sidebar } from "~/components/ui/sidebar";
 import { AppProviders } from "~/providers/app-providers";
 
 export const AppLayout = ({ children }: PropsWithChildren) => {
+  useHotkeys([
+    ["mod+J", () => console.log("Toggle color scheme")],
+    ["ctrl+K", () => console.log("Trigger search")],
+    ["ctrl+J", () => console.log("Trigger search")],
+    ["alt+mod+shift+X", () => console.log("Rick roll")],
+  ]);
+
   return (
     <AppProviders>
       <section className="sm:ml-64">
