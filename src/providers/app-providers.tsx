55dccf7e4410eb24c18054b6a7699e8b7faead68
davialcantaraa <dxvialcantara@gmail.com>
2023-04-05 18:00:06 -0300

    feat: add react query

--- src/providers/app-providers.tsx
+++ src/providers/app-providers.tsx
@@ -1,6 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import { queryClient } from "~/services/react-query";
+import { DocumentProvider } from "./document-provider";
 import { WindowProvider } from "./window-provider";
 
 export const AppProviders = ({ children }: PropsWithChildren) => {
-  return <WindowProvider>{children}</WindowProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WindowProvider>
+        <DocumentProvider>{children}</DocumentProvider>
+      </WindowProvider>
+    </QueryClientProvider>
+  );
 };
